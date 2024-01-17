88480034071e46bb9beb1fce875f2aac99b2790b
pc01\aluno <informatica@eticalgarve.com>
2024-01-17 21:58:12 +0000

    Commit na aula

---
+++ aula3/app.js
@@ -0,0 +1,126 @@
+//let a = "praia";
+
+//if (a == "praia") {
+   // console.log("é praia");
+//} else {}
+    //console.log("não é praia");
+//}
+///////////////////////
+
+    //let a = 10;
+//if (a === "10") {
+   // console.log(true);
+//} else {
+   // console.log(false);
+//}
+
+//a == "10" ? console.log(true) : console.log(false);
+//}
+////////////////////////
+
+//switch(a) {
+    //case 10:
+        //console.log(true);
+        //break;
+    //case "10":
+        //console.log(false);
+       //break;
+    //case "20":
+        //console.log(false);
+        //break;
+    //case "40":
+        //console.log(false);
+        //break;
+        //default:
+            //console.log(true);
+//}
+////////////////////////////
+
+//let a =10;
+//let b = "100";
+
+//if(a ==="10" && b == "100") {
+   //console.log (true);
+//} else {
+    //console.log(false);
+//}
+
+//if(a ==="10" || b == "100") {
+    //console.log (true);
+//} else {
+    //console.log(false);
+//}
+//////////////////////////////////
+
+//let a = 0;
+
+//while (a < 20) {
+    //console.log(a);
+    //a = a + 1;
+//}
+
+//WHILE é uma função Loop//
+
+//let a = 0;
+
+//while (a < 20) {
+    //console.log("a", a);
+    //a++;
+//}
+/////////////////////////////
+//let arr = ["praia", "cidade", "campo", "floresta","montanha", 10, 100];
+
+//for(let i = 0; i < arr.length; i++) {
+    //console.log(arr[i]);
+//}
+/////////////////////////////
+
+//let arr = ["praia", "cidade", "campo", "floresta","montanha", 10, 100];
+
+//for(let i = arr.length-1; i >= 0; i--) {
+    //console.log(arr[i]);
+//}
+/////////////////////////////
+//const PI =3.14;
+//let arr = [20,30,40,10,100];
+//console.log(arr);
+
+//for(let i = arr.length-1; i >= 0; i--) {
+    //arr[i] = arra[i] *PI;
+//}
+///////////////////////////////
+
+//const PI =3.14;
+//let arr = [20,30,40,10,100];
+//arr[0]= arr[0]* PI;
+//arr[1]= arr[1]* PI;
+//arr[2]= arr[2]* PI;
+
+//console.log(arr);
+
+//for(let i = arr.length-1; i >= 0; i--) {
+    //console.log(arr[i]);
+//}
+//FOR é muito usado para arrays//
+
+//arr.forEach(element, i) => {
+    //console.log(element);
+    //console.log(i);
+//});
+///////////////////////////
+let obj ={
+    name:"Jessie",
+    age: 26,
+    gender:"woman",
+}
+
+console.log(obj["name"])
+
+for ( const key in obj) {
+    //if (Object.hasOwnPropety.call(object, key)) {
+       //const element =object[key];
+    //}
+
+//console.log(obj[key]);
+}
+
