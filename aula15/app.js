f7693ad10a72c74436e99988c30f7d622fe46e83
pc01\aluno <informatica@eticalgarve.com>
2024-02-05 21:42:12 +0000

    Aula15

---
+++ aula15/app.js
@@ -0,0 +1,24 @@
+let a = 12;
+
+let arr = [12,23,54,56, () => {
+    console.log("hello");
+    // return "hello";
+},  "string A", true, {
+    type: "car"
+}]
+
+
+// window.onload = () => {
+//     console.log("loaded");
+// }
+
+// window.addEventListener("keydown", () => {
+//     console.log("iowuer");
+// })
+
+// window.onkeydown = () => {
+//     console.log("key down");
+// }
+
+ console.log(arr);
+//  console.log(arr[4]());
